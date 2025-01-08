c776b214b4e34486e77bc84cf052ed95d0935dd1
Rakibul_islam <130771331+Ph0Enix04@users.noreply.github.com>
2025-01-08 20:04:24 +0600

    Add files via upload

---
+++ SPOJ/start.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+//using namespace __gnu_pbds;
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+int dx[] = {-1,1, 0, 0, -1,-1, 1, 1};
+int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
+#define ll long long
+#define endl '\n'
+const unsigned int MX = 1000000007;
+//ll gcd(ll A, ll B) { return __gcd(A, B); }
+//ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+ 
+void solve() 
+{
+	
+}
+ 
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll TC = 1;
+	//cin >> TC;
+    while (TC--) 
+    {
+        solve();
+    }
+}
